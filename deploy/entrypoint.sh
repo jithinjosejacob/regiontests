136b44d296472f292feec488b57c86f0b27b96cb
Udit Gaurav <35391335+uditgaurav@users.noreply.github.com>
2021-03-26 07:19:39 +0530

    Update entrypoint.sh

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -4,8 +4,8 @@
 
 set -e
 
-# Setting up service account to run the jobs
-kubectl apply -f deploy/rbac.yaml
+## Setting up service account to run the jobs
+#kubectl apply -f deploy/rbac.yaml
 
 # Declare an array of all target regions to run the test
  declare -a regions=("au" "us" "ca")
@@ -22,7 +22,7 @@ kubectl create -f deploy/cypress-all-region-test.yaml
 rm -rf deploy/cypress-all-region-test.yaml
 
 # Waiting for all job pods to get in running state
-kubectl wait --for=condition=Ready pods -l app=cypress-test --request-timeout 3m
+kubectl wait --for=condition=Ready pods -l app=cypress-test --request-timeout 5m
 
 # Printing logs for all the pods
 kubectl logs -f -l app=cypress-test
