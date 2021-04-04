e6b408474fec4064b864557997c103f520761fbe
udit <udit@chaosnative.com>
2021-04-04 08:43:03 +0530

    Setup ENVs to the pod
    Signed-off-by: udit <udit@chaosnative.com>

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -6,13 +6,31 @@ set -e
 
 NS="training"
 
+## Install dependencies to run j2 command
+apt-get update && apt-get install -y --no-install-recommends python-pip
+pip install j2
+
+## Exporting default values for the given variables
+export CYPRESS_requestApiKey="${CYPRESS_requestApiKey:-default}"
+export CYPRESS_onboardingUrl="${CYPRESS_onboardingUrl:-default}"
+export CYPRESS_apiUrl="${CYPRESS_apiUrl:-default}"
+export CYPRESS_apiHostUrl="${CYPRESS_apiHostUrl:-default}"
+export AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION:-default}"
+export AWS_REGION="${AWS_REGION:-default}"
+export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID:-default}"
+export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY:-default}"
+export AWS_REGION="${AWS_REGION:-default}"
+
+## Running j2 command to setup variables in the yaml
+j2 cypress-test.j2 > cypress-test.yaml
+
 # Declare an array of all target regions to run the test
 declare -a regions=("au" "us" "ca" "au" "us")
 
 touch deploy/cypress-all-region-test.yaml
-# Prepare the test jobs for all the regions
+# Prepare the test pods for all the regions
 for val in ${regions[@]}; do
-   sed -i "/name: CYPRESS_region/{n;s/.*/              value: "$val"/}" deploy/cypress-test.yaml
+   sed -i "/name: CYPRESS_region/{n;s/.*/          value: "$val"/}" deploy/cypress-test.yaml
    cat deploy/cypress-test.yaml >> deploy/cypress-all-region-test.yaml
 done
 
@@ -27,7 +45,6 @@ declare -a array=$( kubectl get pods -n $NS -l run=cypress-test-unique -o=jsonpa
 RETRIES=50
 for i in ${array[@]}; do
    while ([ "$( kubectl get pods $i -n $NS -o=jsonpath='{.status.phase}' )" != "Succeeded" ]); do
-      kubectl get pods -n $NS
       echo "Waiting for \"$i\" to get completed ... $RETRIES retries left."
       sleep 10
       RETRIES=$((RETRIES-1))
@@ -75,7 +92,8 @@ else
 fi
 
 # Cleanup the test pods
-kubectl delete jobs -n $NS -l run=cypress-test-unique
+kubectl delete pods -n $NS -l run=cypress-test-unique
+rm -rf deploy/cypress-test.yaml
 if [ "${INDEX}" != "1" ];then
    echo "Some cypress tests failed please check the above logs for the details"
    exit 1
